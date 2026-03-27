28016ba8847e880f7c18d77c5b8a054bbf2a93e2
SteadyOsi <Josiahstedman@gmail.com>
2026-03-27 19:20:09 +1100

    finished up to step 5
    Not inclusive

--- brain-stack/src/app/layout/shell/shell.ts
+++ brain-stack/src/app/layout/shell/shell.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Navbar } from '../navbar/navbar';
 
 @Component({
   selector: 'app-shell',
-  imports: [],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './shell.html',
   styleUrl: './shell.css',
 })
