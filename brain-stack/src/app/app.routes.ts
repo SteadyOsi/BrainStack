74e64888c75373f8beeacaf8e21dcfae50d9fe81
SteadyOsi <Josiahstedman@gmail.com>
2026-04-06 22:44:38 +1000

    imports

--- brain-stack/src/app/app.routes.ts
+++ brain-stack/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { Shell } from './layout/shell/shell'
-import { Home } from './pages/home/home'
-import { Chip8 } from './features/chip8/chip8'
+import { Shell } from './layout/shell/shell';
+import { Home } from './pages/home/home';
+import { Chip8 } from './features/chip8/chip8';
 
 export const routes: Routes = [
     {
